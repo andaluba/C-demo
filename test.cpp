12670a2a341cc2fa7783adec6d3be5ec4ec42551
andaluba <799630707@qq.com>
2020-02-17 10:35:30 +0800

    first commint

---
+++ test.cpp
@@ -0,0 +1,13 @@
+//001
+#include <iostream>
+using namespace std;
+ 
+int main() {
+    // insert code here...
+    int i = 1;
+    cout << "欢迎" << endl;
+    cout << 'a' << endl;
+    cout << "i=" << i << endl;
+    cout << '\n';
+    return 0;
+}
